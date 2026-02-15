3939c16d2c2aead9ee47973260551a57ea88eb45
Jack Anderson <jackanderson02@outlook.com>
2026-02-15 01:54:52 +0000

    feat: upstream origin

--- plugin/plugin.lua
+++ plugin/plugin.lua
@@ -60,7 +60,7 @@ vim.keymap.set("n", "<leader>wa", function()
 
     -- Put new worktrees in git root dir
     local worktree_path = git.gitroot_dir() .. "/" .. worktree_name
-    worktree.create_worktree(worktree_path, worktree_name, worktree_name)
+    worktree.create_worktree(worktree_path, worktree_name, "origin/" .. worktree_name)
 end)
 
 vim.keymap.set("n", "<leader>ws", function()
