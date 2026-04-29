bff0fbda8560454dd4927a72a7cecf8ae9cbb26c
Jack Anderson <jack.anderson@bjss.com>
2026-04-29 11:16:53 +0100

    feat: fetch all panes - seems to work

--- lua/git-worktree/tmux.lua
+++ lua/git-worktree/tmux.lua
@@ -22,31 +22,43 @@ local function is_shell_only(command)
     local default_shell = vim.env.SHELL
     if not default_shell then
         -- Cannot determine shell, be conservative and don't change CWD
+        print(string.format('[git-worktree] SHELL not set, treating pane as non-shell (cmd: %s)', command))
         return false
     end
     local shell_name = default_shell:match('([^/]+)$')
     local cmd_name = command:match('([^/]+)$') or command
-    return cmd_name == shell_name
+    local is_shell = cmd_name == shell_name
+    print(string.format('[git-worktree] Shell check: cmd=%s, shell=%s, is_shell=%s', cmd_name, shell_name, tostring(is_shell)))
+    return is_shell
+end
+
+---Get the current pane ID from environment variable
+---@return string|nil
+local function get_current_pane_id()
+    local tmux_pane = vim.env.TMUX_PANE
+    return tmux_pane
 end
 
 ---@return table, table, table
---- table of the inactive tmux pane IDs
---- table of cwd's of all inactive panes... in the same order as the first table.
---- table of commands of all inactive panes... in the same order as the first table.
-function M.get_inactive_panes_and_cwds()
-    local tmux_active = 'ACTIVE'
-    local inactive_panes = {}
-    local inactive_cwds = {}
-    local inactive_commands = {}
+--- table of the tmux pane IDs (excluding current pane)
+--- table of cwd's of all panes... in the same order as the first table.
+--- table of commands of all panes... in the same order as the first table.
+function M.get_session_panes_and_cwds()
+    local current_pane_id = get_current_pane_id()
+    local panes = {}
+    local pane_cwds = {}
+    local pane_commands = {}
 
+    print('[git-worktree] Fetching panes from current tmux session...')
+    print(string.format('[git-worktree] Current pane ID (will be skipped): %s', tostring(current_pane_id)))
     local sep = ','
     local job = Job:new {
         command = 'tmux',
         args = {
             'list-panes',
-            '-a',
+            '-s',  -- Limit to current session only
             '-F',
-            string.format('#{pane_id},#{?pane_active,%s, },#{pane_current_path},#{pane_current_command}', tmux_active),
+            string.format('#{pane_id},#{pane_current_path},#{pane_current_command}'),
         },
         cwd = vim.loop.cwd(),
         on_stdout = function(_, data, _)
@@ -55,19 +67,23 @@ function M.get_inactive_panes_and_cwds()
                 for token in string.gmatch(data, '([^' .. sep .. ']+)') do
                     table.insert(tmux_out, token)
                 end
-                if #tmux_out ~= 4 then
+                if #tmux_out ~= 3 then
                     error('Did not collect sufficient information from tmux.' .. vim.inspect(tmux_out))
                     return
                 end
 
                 local pane_id = tmux_out[1]
-                local active = tmux_out[2]
-                local pane_cwd = tmux_out[3]
-                local pane_cmd = tmux_out[4]
-                if active ~= tmux_active then
-                    table.insert(inactive_panes, pane_id)
-                    table.insert(inactive_cwds, pane_cwd)
-                    table.insert(inactive_commands, pane_cmd)
+                local pane_cwd = tmux_out[2]
+                local pane_cmd = tmux_out[3]
+
+                -- Skip the current pane (where Neovim is running)
+                if pane_id == current_pane_id then
+                    print(string.format('[git-worktree] Skipping current pane: id=%s, cwd=%s, cmd=%s', pane_id, pane_cwd, pane_cmd))
+                else
+                    print(string.format('[git-worktree] Found pane: id=%s, cwd=%s, cmd=%s', pane_id, pane_cwd, pane_cmd))
+                    table.insert(panes, pane_id)
+                    table.insert(pane_cwds, pane_cwd)
+                    table.insert(pane_commands, pane_cmd)
                 end
             end
         end,
@@ -75,9 +91,10 @@ function M.get_inactive_panes_and_cwds()
 
     local res, ret = job:sync()
     if ret ~= 0 then
-        error('Failed get_inactive_panes_and_cwds: ' .. ret .. vim.inspect(res))
+        error('Failed get_session_panes_and_cwds: ' .. ret .. vim.inspect(res))
     end
-    return inactive_panes, inactive_cwds, inactive_commands
+    print(string.format('[git-worktree] Total panes found (excluding current): %d', #panes))
+    return panes, pane_cwds, pane_commands
 end
 
 ---@param pane_id string the ID of the pane to send the command to
@@ -92,9 +109,18 @@ function M.send_keys(pane_id, cmd)
 end
 
 function M.change_session_cwds(prev_path, path)
-    local inactive_panes, cwds, commands = M.get_inactive_panes_and_cwds()
-    for idx, pane_id in ipairs(inactive_panes) do
-        if cwds[idx] == prev_path and is_shell_only(commands[idx]) then
+    print(string.format('[git-worktree] Changing session CWDs from %s to %s', prev_path, path))
+    local panes, cwds, commands = M.get_session_panes_and_cwds()
+    for idx, pane_id in ipairs(panes) do
+        local pane_cwd = cwds[idx]
+        local pane_cmd = commands[idx]
+        print(string.format('[git-worktree] Checking pane %s: cwd=%s (expected %s), cmd=%s', pane_id, pane_cwd, prev_path, pane_cmd))
+        if pane_cwd ~= prev_path then
+            print(string.format('[git-worktree] SKIPPING pane %s: CWD mismatch (pane: %s, expected: %s)', pane_id, pane_cwd, prev_path))
+        elseif not is_shell_only(pane_cmd) then
+            print(string.format('[git-worktree] SKIPPING pane %s: not running shell (running: %s)', pane_id, pane_cmd))
+        else
+            print(string.format('[git-worktree] CHANGING pane %s: cd %s', pane_id, path))
             M.send_keys(pane_id, string.format('cd %s', path))
             -- Clear shell
             M.send_keys(pane_id, 'clear')
