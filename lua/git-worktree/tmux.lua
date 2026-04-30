d71a0843ae8f794e0a8c57ee5c77451f1d4048f1
Jack Anderson <jack.anderson@bjss.com>
2026-04-30 11:19:44 +0100

    chore: remove debug

--- lua/git-worktree/tmux.lua
+++ lua/git-worktree/tmux.lua
@@ -22,13 +22,11 @@ local function is_shell_only(command)
     local default_shell = vim.env.SHELL
     if not default_shell then
         -- Cannot determine shell, be conservative and don't change CWD
-        print(string.format('[git-worktree] SHELL not set, treating pane as non-shell (cmd: %s)', command))
         return false
     end
     local shell_name = default_shell:match('([^/]+)$')
     local cmd_name = command:match('([^/]+)$') or command
     local is_shell = cmd_name == shell_name
-    print(string.format('[git-worktree] Shell check: cmd=%s, shell=%s, is_shell=%s', cmd_name, shell_name, tostring(is_shell)))
     return is_shell
 end
 
@@ -49,8 +47,6 @@ function M.get_session_panes_and_cwds()
     local pane_cwds = {}
     local pane_commands = {}
 
-    print('[git-worktree] Fetching panes from current tmux session...')
-    print(string.format('[git-worktree] Current pane ID (will be skipped): %s', tostring(current_pane_id)))
     local sep = ','
     local job = Job:new {
         command = 'tmux',
@@ -78,9 +74,7 @@ function M.get_session_panes_and_cwds()
 
                 -- Skip the current pane (where Neovim is running)
                 if pane_id == current_pane_id then
-                    print(string.format('[git-worktree] Skipping current pane: id=%s, cwd=%s, cmd=%s', pane_id, pane_cwd, pane_cmd))
                 else
-                    print(string.format('[git-worktree] Found pane: id=%s, cwd=%s, cmd=%s', pane_id, pane_cwd, pane_cmd))
                     table.insert(panes, pane_id)
                     table.insert(pane_cwds, pane_cwd)
                     table.insert(pane_commands, pane_cmd)
@@ -93,7 +87,6 @@ function M.get_session_panes_and_cwds()
     if ret ~= 0 then
         error('Failed get_session_panes_and_cwds: ' .. ret .. vim.inspect(res))
     end
-    print(string.format('[git-worktree] Total panes found (excluding current): %d', #panes))
     return panes, pane_cwds, pane_commands
 end
 
@@ -109,18 +102,13 @@ function M.send_keys(pane_id, cmd)
 end
 
 function M.change_session_cwds(prev_path, path)
-    print(string.format('[git-worktree] Changing session CWDs from %s to %s', prev_path, path))
     local panes, cwds, commands = M.get_session_panes_and_cwds()
     for idx, pane_id in ipairs(panes) do
         local pane_cwd = cwds[idx]
         local pane_cmd = commands[idx]
-        print(string.format('[git-worktree] Checking pane %s: cwd=%s (expected %s), cmd=%s', pane_id, pane_cwd, prev_path, pane_cmd))
         if pane_cwd ~= prev_path then
-            print(string.format('[git-worktree] SKIPPING pane %s: CWD mismatch (pane: %s, expected: %s)', pane_id, pane_cwd, prev_path))
         elseif not is_shell_only(pane_cmd) then
-            print(string.format('[git-worktree] SKIPPING pane %s: not running shell (running: %s)', pane_id, pane_cmd))
         else
-            print(string.format('[git-worktree] CHANGING pane %s: cd %s', pane_id, path))
             M.send_keys(pane_id, string.format('cd %s', path))
             -- Clear shell
             M.send_keys(pane_id, 'clear')
